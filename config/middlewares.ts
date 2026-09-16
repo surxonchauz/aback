31fea8d7ee4278b3fd59ab2a2d657e1951b7f01c
surxonchauz <surxonchauz@gmail.com>
2026-09-16 15:18:20 +0000

    feat: setup cloudflare r2 media storage

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,9 +1,36 @@
 import type { Core } from '@strapi/strapi';
 
-const config: Core.Config.Middlewares = [
+const config = ({ env }: Core.Config.Shared.ConfigParams): Core.Config.Middlewares => [
   'strapi::logger',
   'strapi::errors',
-  'strapi::security',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'dl.airtable.com',
+            '*.r2.cloudflarestorage.com',
+            env('R2_PUBLIC_URL'),
+          ],
+          'media-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'dl.airtable.com',
+            '*.r2.cloudflarestorage.com',
+            env('R2_PUBLIC_URL'),
+          ],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::query',
